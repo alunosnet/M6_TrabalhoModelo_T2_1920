37007571a1da0179c6eb1bd3670a292a5e9316a9
Paulo Ferreira <alunosnet@gmail.com>
2019-12-17 08:49:16 +0000

    Páginas para consultas

---
+++ M6_TrabalhoModelo_T2_1920_WIP/Notas/pesquisar.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M6_TrabalhoModelo_T2_1920_WIP.Notas
+{
+    public partial class pesquisar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
